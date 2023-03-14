ad63175dcc99d64790c60a913fbd3c8f7d91ee03
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-03-14 23:36:21 +0100

    test

--- watched_it/src/views/HomeView.vue
+++ watched_it/src/views/HomeView.vue
@@ -32,30 +32,21 @@ onMounted(() => {
     <br>
     <br>
     <label class="mdc-text-field mdc-text-field--outlined">
-  <span class="mdc-notched-outline">
-    <span class="mdc-notched-outline__leading"></span>
-    <span class="mdc-notched-outline__notch">
-      <span class="mdc-floating-label" id="my-label-id">Your Name</span>
-    </span>
-    <span class="mdc-notched-outline__trailing"></span>
-  </span>
-  <input type="text" class="mdc-text-field__input" aria-labelledby="my-label-id">
-</label>
+      <span class="mdc-notched-outline">
+        <span class="mdc-notched-outline__leading"></span>
+        <span class="mdc-notched-outline__notch">
+          <span class="mdc-floating-label" id="my-label-id">Your Name</span>
+        </span>
+        <span class="mdc-notched-outline__trailing"></span>
+      </span>
+      <input type="text" class="mdc-text-field__input" aria-labelledby="my-label-id">
+    </label>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @use "@material/button/styles";
-  @use "@material/floating-label/mdc-floating-label";
-  @use "@material/line-ripple/mdc-line-ripple";
-  @use "@material/notched-outline/mdc-notched-outline";
-  @use "@material/textfield";
-  @include textfield.core-styles;
   @import "@/assets/styles/colors.scss";
-  
-  .mdc-button--raised{
-    background-color: $black !important;
-    color: $white;
-    border-radius: 50px;
-  }
+  @import "@/assets/styles/input.scss";
+
 </style>
