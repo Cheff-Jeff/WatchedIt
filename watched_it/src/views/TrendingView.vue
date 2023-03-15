f45e6519119b228a62d990ee7fe80d642ec009a3
Mattijs Pronk <456410@student.fontys.nl>
2023-03-15 15:33:10 +0100

    Card created

---
+++ watched_it/src/views/TrendingView.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <div class="card-container">
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr/>
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr/>
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import '../assets/styles/trending.css';
+</style>
