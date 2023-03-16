f3433447103e312d0b5677649ec81dd2776704eb
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-03-16 23:07:54 +0100

    update menu

--- watched_it/src/components/SimpleHeader.vue
+++ watched_it/src/components/SimpleHeader.vue
@@ -33,8 +33,39 @@ const props = defineProps({
         </div>
 
     </div>
-    <div class="menu">
-        hi
+    <div class="menu-wrapper">
+        <div class="menu">
+            <div class="close-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+                </svg>
+            </div>
+            <div class="inner-wrap">
+                <div class="logo-wrap">
+                    <img src="@/assets/logo.png" alt="logo">
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <RouterLink :to="{name: 'account'}">
+                                Account
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink :to="{name: 'trending'}">
+                                Trending
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <a href="#">Watch list</a>
+                        </li>
+                        <li>
+                            <a href="#">Comig soon</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
 </template>
 
