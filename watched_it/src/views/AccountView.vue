ab02b5b128fec64026b648529ba568d5023ac48a
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-03-17 13:24:37 +0100

    update account

--- watched_it/src/views/AccountView.vue
+++ watched_it/src/views/AccountView.vue
@@ -1,15 +1,222 @@
 <script setup lang="ts">
 import SimpleHeader from '@/components/SimpleHeader.vue';
+import {MDCRipple} from '@material/ripple';
+import { onMounted } from 'vue';
+onMounted(() => { 
+  const buttons = document.getElementsByClassName('mdc-button');
+  if(buttons){
+    for(let btn of buttons){
+      const buttonRipple = new MDCRipple(btn);
+    }
+  }
+});
 </script>
 
 <template>
   <SimpleHeader currentTitle="Account" />
   <section class="section-pagewrap">
-    <h1>Hi</h1>
+    <div class="container">
+      <section class="greed">
+        <div class="row">
+          <div class="col-8">
+            <div class="person">
+              <h1>Hello there,</h1>
+              <span>Jeffrey Nijkamp</span>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="mdc-touch-target-wrapper btn-wrap white">
+              <button class="mdc-button mdc-button--raised">
+                <span class="mdc-button__ripple"></span>
+                <span class="mdc-button__touch"></span>
+                <span class="mdc-button__label">Edit account</span>
+              </button>
+            </div>
+            <div class="mdc-touch-target-wrapper btn-wrap">
+              <button class="mdc-button mdc-button--raised">
+                <span class="mdc-button__ripple"></span>
+                <span class="mdc-button__touch"></span>
+                <span class="mdc-button__label">Download app</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="nav">
+        <div class="row justify-content-around">
+          <div class="col-5">
+            <div class="link-wrapper">
+              <router-link :to="{name: 'signUp'}" class="link">
+                Recomandations
+              </router-link>
+            </div>
+          </div>
+          <div class="col-5">
+            <div class="link-wrapper">
+              <router-link :to="{name: 'signUp'}" class="link">
+                Watch lists
+              </router-link>
+            </div>
+          </div>
+          <div class="col-5">
+            <div class="link-wrapper">
+              <router-link :to="{name: 'signUp'}" class="link">
+                Friends
+              </router-link>
+            </div>
+          </div>
+          <div class="col-5">
+            <div class="link-wrapper">
+              <router-link :to="{name: 'signUp'}" class="link">
+                Favorites
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="movies">
+        <div class="section-title">
+            <h1>Movies for you</h1>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+        </div>
+      </section>
+      <section class="shows">
+        <div class="section-title">
+            <h1>Shows for you</h1>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="movie-image-container">
+                    <img class="movie-image" src="https://imgur.com/rYSzWYZ.png">
+                </div>
+                <div class="info-box">
+                    <h1>Halloween Kills</h1>
+                    <hr />
+                    <p>Released at: 1978-04-17</p>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/colors.scss";
+@import "@/assets/styles/components/buttons.scss";
 @import "@/assets/styles/pages/account.scss";
+@import "@/assets/styles/components/cards.scss"
 </style>
