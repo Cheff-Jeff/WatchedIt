f97bcabdc39092bd14db4c2cd3803bbdf8e31561
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-03-20 21:26:17 +0100

    update service-worker

--- watched_it/vue.config.js
+++ watched_it/vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Watched It',
+    themeColor: '#B29056',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: './src/service-worker.js',
+      exclude: [
+        ".htaccess"
+      ]
+      // ...other Workbox options...
+    }
+  }
 })
