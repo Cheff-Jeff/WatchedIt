4025f19516781b54c9c57c578642dcaa8ef0d293
Mattijs Pronk <456410@student.fontys.nl>
2023-03-29 19:50:47 +0200

    Share-btn on movie/show details

--- watched_it/src/views/DetailView.vue
+++ watched_it/src/views/DetailView.vue
@@ -133,7 +133,19 @@ defineExpose({ details, favorite, favoriteId, route })
     </section>
     <section class="cast">
       <div class="container">
-        <h2>Actors</h2>
+        <div class="row">
+          <div class="col-10">
+            <h2>Actors</h2>
+          </div>
+          <div class="col-2">
+            <svg v-on:click="shareItem(details!.title, details!.poster)" class="share-icon" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z" />
+            </svg>
+          </div>
+        </div>
+
         <div class="cast-wrap">
           <div v-for="actor in details?.cast" :key="actor.name" class="cast-member">
             <img v-if="actor.photo" :src="`https://image.tmdb.org/t/p/w500/${actor.photo}`">
@@ -212,14 +224,23 @@ export default defineComponent({
     let result = [] as any;
     result = await getCollectionList(JSON.parse(localStorage.getItem('user') || '{}'));
 
-    
+
     for (let i = 0; i < result._value.length; i++) {
-      if(result._value[i].addMovieDeadLine.replace('-', '').replace('-', '') > new Date().toLocaleDateString('en-GB').replace('/', '').replace('/', '')){
+      if (result._value[i].addMovieDeadLine.replace('-', '').replace('-', '') > new Date().toLocaleDateString('en-GB').replace('/', '').replace('/', '')) {
         this.movieshowList.push(result._value[i])
       }
     }
   },
   methods: {
+    shareItem(title: string, photo:string) {
+      const shareData = {
+        title: "WatchedIt",
+        text: title,
+        // url: `https://image.tmdb.org/t/p/w500/${photo}`,
+        url: window.location.href,
+      };
+      navigator.share(shareData)
+    },
     togglePopup() {
       this.modalToggle = !this.modalToggle;
     },
@@ -276,8 +297,6 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss" scoped>
-@import "@/assets/styles/colors.scss";
+<style lang="scss" scoped>@import "@/assets/styles/colors.scss";
 @import "@/assets/styles/components/buttons.scss";
-@import "@/assets/styles/pages/details.scss";
-</style>
+@import "@/assets/styles/pages/details.scss";</style>
